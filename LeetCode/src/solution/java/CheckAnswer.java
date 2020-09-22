dfe1b5cb0ad50f1926ce46b55b0eeca0d1c54eee
Sadamune <zwiebelshi@gmail.com>
2020-09-22 21:16:29 +0900

    complete myAtoi

--- LeetCode/src/solution/java/CheckAnswer.java
+++ LeetCode/src/solution/java/CheckAnswer.java
@@ -3,8 +3,13 @@ package solution.java;
 public class CheckAnswer {
 
 	public static void main(String[] args) {
-		Solution s = new Solution();
-		String[] inputMyAtoi = {"   +100 ", "-2312312312", "123123123123aabb"};
+	    MyAtoi s = new MyAtoi();
+		String[] inputMyAtoi = {"   +100 ",
+		        "-2312312312",
+		        "123123123123aabb",
+		        "  -0000000123456789",
+		        "    0000000000000   ",
+		        "    +0a32"};
 		for (String input : inputMyAtoi) {
 			System.out.println(s.myAtoi(input));
 		}
