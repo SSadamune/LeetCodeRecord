8be1e4da33fddbabb3c90fc6853c919f98fa4d04
Sadamune <zwiebelshi@gmail.com>
2020-09-28 00:23:26 +0900

    complete java/generateParenthesis

--- LeetCodeJava/src/questions/generateParenthesis/Solution.java
+++ LeetCodeJava/src/questions/generateParenthesis/Solution.java
@@ -1,3 +1,7 @@
+/*
+ * https://leetcode.com/problems/generate-parentheses/
+ * 2020.09.27
+ */
 package questions.generateParenthesis;
 
 import java.util.ArrayList;
@@ -8,17 +12,32 @@ class Solution {
     private int length;
 
     public List<String> generateParenthesis(int n) {
-        this.ans.clear();
-        this.length = n * 2;
         this.ans = new ArrayList<String>();
-
-        addStringToAns("", 0, 0, 0);
-
+        this.length = n * 2;
+        addStringToAns("", 0, 0);
         return this.ans;
     }
 
-    public void addStringToAns(String str, int index, int left, int right) {
-        if (index == length) this.ans.add(str);
+    public void addStringToAns(String str, int left, int right) {
+        // add str to this.ans
+        if (left + right == this.length) this.ans.add(str);
 
+        // the number of '(' & ')' are the same, must add '('
+        else if (left == right) addStringToAns(str + "(", left + 1, right);
+
+        // the number of '(' is enough, fill the rest ')'
+        else if (left == this.length / 2){
+            StringBuilder closingBracket = new StringBuilder();
+            while (right < left) {
+                closingBracket.append(")");
+                right++;
+            }
+            addStringToAns(str + closingBracket, left, right);
+
+        // add '(' or ')'
+        } else {
+            addStringToAns(str + "(", left + 1, right);
+            addStringToAns(str + ")", left, right + 1);
+        }
     }
 }
