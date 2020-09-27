540a514d59e388d4d33e1546017a6c77bd3d0d75
Sadamune <zwiebelshi@gmail.com>
2020-09-28 00:52:48 +0900

    add reference solutions for java/generateParenthesis

--- LeetCodeJava/src/questions/generateParenthesis/Solution.java
+++ LeetCodeJava/src/questions/generateParenthesis/Solution.java
@@ -1,4 +1,4 @@
-/*
+/**
  * https://leetcode.com/problems/generate-parentheses/
  * 2020.09.27
  */
@@ -11,6 +11,7 @@ class Solution {
     private List<String> ans;
     private int length;
 
+    // my answer
     public List<String> generateParenthesis(int n) {
         this.ans = new ArrayList<String>();
         this.length = n * 2;
@@ -39,5 +40,73 @@ class Solution {
             addStringToAns(str + "(", left + 1, right);
             addStringToAns(str + ")", left, right + 1);
         }
+    }
+
+    // reference answer: dfs
+    public List<String> generateParenthesisDfs(int n) {
+        List<String> res = new ArrayList<>();
+        // special case
+        if (n == 0) return res;
+
+        // dfs, find possible results
+        dfs("", n, n, res);
+        return res;
+    }
+
+    /**
+     * @param curStr a string as current result
+     * @param left   how many open brackets left for use
+     * @param right  how many close brackets left for use
+     * @param res    result set
+     */
+    private void dfs(String curStr, int left, int right, List<String> res) {
+        // no brackets for use, add curStr
+        if (left == 0 && right == 0) {
+            res.add(curStr);
+            return;
+        }
+
+        // Pruning: used ')' more than '('
+        if (left > right) {
+            return;
+        }
+
+        // use a new '('
+        if (left > 0) {
+            dfs(curStr + "(", left - 1, right, res);
+        }
+
+        // use a new ')'
+        if (right > 0) {
+            dfs(curStr + ")", left, right - 1, res);
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        // 这里 dp 数组我们把它变成列表的样子，方便调用而已
+        List<List<String>> dp = new ArrayList<>(n);
+
+        List<String> dp0 = new ArrayList<>();
+        dp0.add("");
+        dp.add(dp0);
+
+        for (int i = 1; i <= n; i++) {
+            List<String> cur = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = dp.get(j);
+                List<String> str2 = dp.get(i - 1 - j);
+                for (String s1 : str1) {
+                    for (String s2 : str2) {
+                        // 枚举右括号的位置
+                        cur.add("(" + s1 + ")" + s2);
+                    }
+                }
+            }
+            dp.add(cur);
+        }
+        return dp.get(n);
     }
 }
